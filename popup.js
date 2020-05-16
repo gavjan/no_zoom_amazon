647d99c9f9005509d35e4394ed7476ca2b6f99a1
Gevorg Chobanyan <gevochobanyan@gmail.com>
2020-05-16 20:48:37 +0400

    botch

--- popup.js
+++ popup.js
@@ -1,23 +1,9 @@
-// callback executed when canvas was found
 function handleCanvas(canvas) { 
   var myobj = document.getElementById("magnifierLens");
-  myobj.remove();
+  if(myobj){
+    myobj.remove();
+  }
+  
 }
 
-// set up the mutation observer
-var observer = new MutationObserver(function (mutations, me) {
-  // `mutations` is an array of mutations that occurred
-  // `me` is the MutationObserver instance
-  var canvas = document.getElementById('magnifierLens');
-  if (canvas) {
-    handleCanvas(canvas);
-    me.disconnect(); // stop observing
-    return;
-  }
-});
-
-// start observing
-observer.observe(document, {
-  childList: true,
-  subtree: true
-});
+window.setInterval(handleCanvas, 1000);
